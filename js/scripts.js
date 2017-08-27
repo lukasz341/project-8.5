5b670e4d02cc7411a98bb8602177813c22e9e5a7
lukasz341 <lukasz341@tlen.pl>
2017-08-27 14:58:18 +0200

    wyrzystanie zmiennych- pole trójkąta

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+var a=prompt ('Tape a'),
+	h=prompt ('Tape h'),
+	triangleArea;
+	
+triangleArea=a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h +' is equal to: ' + triangleArea);
